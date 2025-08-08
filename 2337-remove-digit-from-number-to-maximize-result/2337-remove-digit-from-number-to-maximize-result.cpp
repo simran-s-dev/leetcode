4d5a5e3fcd7b11cc2783b8963de944421c622566
Simran <159306200+simran-s-dev@users.noreply.github.com>
2025-08-08 07:51:19 +0530

    Time: 0 ms (100%), Space: 9.4 MB (19.65%) - LeetHub

---
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDigit(string number, char digit) {
+      string maxResult = "";
+        
+        for (int i = 0; i < number.size(); ++i) {
+            if (number[i] == digit) {
+                string candidate = number.substr(0, i) + number.substr(i + 1);
+                if (candidate > maxResult) {
+                    maxResult = candidate;
+                }
+            }
+        }
+        
+        return maxResult;
+    }
+};
