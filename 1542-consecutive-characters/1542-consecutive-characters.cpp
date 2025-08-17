bfdf7764c1b776bfa6bd9225ff448df58845a9e6
Simranjeet Kaur <159306200+simran-s-dev@users.noreply.github.com>
2025-08-17 21:11:35 +0530

    Time: 0 ms (100%), Space: 9.4 MB (95.28%) - LeetHub

---
+++ 1542-consecutive-characters/1542-consecutive-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int maxi=1; int count =1;
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1]) {
+            count++;
+            }
+            else{
+            count =1;
+            }
+            maxi =max(maxi,count);
+
+        }
+        
+        return maxi;
+    }
+};
